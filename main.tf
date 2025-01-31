9ca260028d420ef1a62342e6b3fa73442e83c3ca
sidhustays424 <sidhustays424@gmail.com>
2025-01-31 16:06:49 -0500

    use module for security group

--- main.tf
+++ main.tf
@@ -14,11 +14,27 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web" "blog"{
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+
+  Vpc_security_group_ids = [module.blog_sg.Vpc_security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "blog_new
+  
+  vpc_id        = data.aws_vpc.default.id
+  
+  ingress_rules       = ["http-80-tcp","http-443-tcp"]
+  ingress_cidr_blocks = [0.0.0.0/0]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = [0.0.0.0/0]
 }
