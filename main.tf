234a1879674337c69442240f730b1307a7d75c25
sidhustays424 <sidhustays424@gmail.com>
2025-01-31 16:32:24 -0500

    SG

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_sg" {
   
   vpc_id        = data.aws_vpc.default.id
   
-  ingress_rules       = ["http-80-tcp","http-443-tcp"]
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
